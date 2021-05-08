816980a64417a2237e6a4063e07305972d7bd4a3
Samuel <astraloverflow@pm.me>
2021-05-08 09:49:19 -0700

    Updating dep versions

--- src/ts/global.d.ts
+++ src/ts/global.d.ts
@@ -1,3 +1,5 @@
+// Prevents Typescript from complaining about custom elements
+// https://caniuse.com/custom-elementsv1
 declare namespace JSX {
   interface IntrinsicElements {
     [elemName: string]: any;
